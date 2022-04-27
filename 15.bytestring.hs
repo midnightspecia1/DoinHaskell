8f1d830313e821d6a2f1c645887ab9b434d4a918
Andrew <ghostsly1@gmail.com>
2022-04-27 08:55:01 +0300

    H10: finished lesson 25

--- 15.bytestring.hs
+++ 15.bytestring.hs
@@ -2,6 +2,9 @@
 
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import qualified Data.Text.Encoding as E
 
 sampleBytes :: B.ByteString 
 sampleBytes = "Hello!"
@@ -16,3 +19,22 @@ bcInt = "6"
 
 toInt :: BC.ByteString -> Int
 toInt = read . BC.unpack
+
+
+nagarjunaBC :: BC.ByteString 
+nagarjunaBC = "नागर्जुनॅ"
+
+nagarjunaText :: T.Text
+nagarjunaText = "првиет"
+
+nagarjunaB :: B.ByteString 
+nagarjunaB = (BC.pack . T.unpack) nagarjunaText
+
+-- Data.Text.Encoding module provide two general functions
+-- E.encodeUtf8 :: T.Text -> BC.ByteString 
+-- E.decodeUtf8 :: BC.ByteString -> T.Text
+-- with them we can safely convert unicode text to bytestrings and vise versa
+
+nagarjunaSafe :: B.ByteString 
+nagarjunaSafe = E.encodeUtf8 nagarjunaText 
+
