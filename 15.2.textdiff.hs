55b9fb4087a6a476ae19e54bfd3c10bfe411f6ff
Andrew <ghostsly1@gmail.com>
2022-05-02 12:56:57 +0300

    H10: ex 25.1 finished

--- 15.2.textdiff.hs
+++ 15.2.textdiff.hs
@@ -1,31 +1,29 @@
+--25.1 difference between characters and bytes (unicode)
 {-#LANGUAGE OverloadedStrings #-}
 
 import qualified Data.ByteString as B
-import qualified Data.ByteString.Char8 as BC
 import qualified Data.Text as T
+import qualified Data.Text.IO as TI
 import qualified Data.Text.Encoding as E
 import System.Environment (getArgs)
 
-getDiff :: T.Text -> B.ByteString -> T.Text 
-getDiff text bString = mconcat ["Characters count: ",
-                                  ls,
-                                  ", ",
-                                  "BytesCount: ",
-                                  lbs,
-                                  ", ",
-                                  "Difference: ",
-                                  difference]
-                    where ls = T.length text
-                          lbs = BC.length bString
-                          difference = ls - lbs
+getDiff :: B.ByteString  -> T.Text 
+getDiff bString = "Characters count: " <>
+                T.pack (show chars)    <>
+                ", "                   <>
+                "BytesCount: "         <>
+                T.pack (show bytes)    <>
+                ", "                   <>
+                "Difference: "         <>
+                T.pack (show difference)
+    where   bytes = B.length bString
+            chars = (T.length . E.decodeUtf8) bString
+            difference = bytes - chars
 
 main :: IO ()
 main = do
     args <- getArgs
     let fileName = head args
-    string <- readFile fileName
-    let text = E.encodeUtf8 (T.pack string)
-    biteString <- B.readFile fileName
-    
-    putStrLn (getDiff string biteString)
+    bstring <- B.readFile fileName
+    TI.putStrLn (getDiff bstring)
     
